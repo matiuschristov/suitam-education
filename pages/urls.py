cdb87ff09c610129753202262537c9354c29ef44
matiuschristov <info@matiuschristov.com>
2024-08-18 00:52:26 +1000

    feat!: update view function names

--- pages/urls.py
+++ pages/urls.py
@@ -4,15 +4,14 @@ from django.conf.urls import handler404
 from . import views
 
 urlpatterns = [
-    path('', views.view_home, name='home'),
-    path('test/', views.view_test, name='test'),
-    path('test/color-scheme/', views.view_test_color_scheme, name='Test.ColorScheme'),
-    path('auth/login/', views.view_user_login, name='Auth.Login'),
-    path('auth/logout/', views.view_user_logout, name='Auth.Logout'),
-    path('profile/', views.view_user_profile, name='UserProfile'),
-    path('overview/', views.view_overview, name='Overview'),
-    path('calendar/', views.view_calendar, name='Calendar'),
-    path('api/class/resources/', views.view_class_resources, name='API.ClassResources'),
+    path('', views.home, name='home'),
+    path('test/color-scheme/', views.test_color_scheme, name='Test.ColorScheme'),
+    path('auth/login/', views.user_login, name='Auth.Login'),
+    path('auth/logout/', views.user_logout, name='Auth.Logout'),
+    path('profile/', views.user_profile, name='UserProfile'),
+    path('overview/', views.app_overview, name='Overview'),
+    path('calendar/', views.app_calendar, name='Calendar'),
+    path('api/class/resources/', views.class_resources, name='API.ClassResources'),
     path('api/dashboard/data/', views.api_dashboard_data, name='API.DashboardData'),
     path('api/user/parents/', views.api_user_parents, name='API.DashboardData'),
     path('api/user/information/', views.api_user_information, name='API.UserInformation'),
