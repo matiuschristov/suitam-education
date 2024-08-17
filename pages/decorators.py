84b757887f6d0f2615006abd7b900aa36ab7017e
matiuschristov <info@matiuschristov.com>
2024-08-18 00:28:25 +1000

    feat: proper error catching with exception class

--- pages/decorators.py
+++ pages/decorators.py
@@ -2,13 +2,30 @@ import urllib.parse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import urllib
+from scraper import intranet
+from pages import utils
+from pages.exception import *
+
+def e_500(error=None):
+    return HttpResponse(error if error else 'An internal error occured please try again', status=500, headers={'Content-Type': 'text/plain'})
 
 def require_authentication(func):
     def wrapper(*args, **kwargs):
         request = args[0]
-        if not request.COOKIES.get('ASP.NET_SessionId') and not request.COOKIES.get('adAuthCookie'):
-            # .format(urllib.parse.quote(request.get_full_path(), safe=''))
+        if not request.COOKIES.get('adAuthCookie'):
             return redirect('/auth/login/')
         else:
-            return func(*args, **kwargs)
+            try:
+                user = intranet.user_information(request)
+                return func(*args, user)
+            except SuitamException as e:
+                if (e.error_code == 'FAILED_AUTH'):
+                    res = redirect('/auth/login/')
+                    utils.delete_cookie(res, 'adAuthCookie')
+                    return res
+                return e_500(error=e)
+            except:
+                return e_500()
+                
+
     return wrapper
