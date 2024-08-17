0ccdd91f931f58df768bca92272bc58667411708
matiuschristov <info@matiuschristov.com>
2024-08-18 00:27:56 +1000

    feat: proper error catching with exception class

---
+++ pages/exception.py
@@ -0,0 +1,7 @@
+class SuitamException(Exception):
+    def __init__(self, message, error_code):
+        super().__init__(message)
+        self.error_code = error_code
+
+    def __str__(self):
+        return f"{self.args[0]} (Error code: {self.error_code})"
