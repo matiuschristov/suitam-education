78c8a8a7b6b8130dd973c6b4b6264d9a403c4c2a
matiuschristov <info@matiuschristov.com>
2024-08-18 10:07:26 +1000

    refactor: error handling in the case that json is corrupted

--- pages/jsonstore.py
+++ pages/jsonstore.py
@@ -1,15 +1,23 @@
 import json
 import os
+from pages.exception import *
 
 class JSONStore():
     def __init__(self):
         pass
     def get(self, type, name):
         with open('/root/projects/suitam/data/{}/{}.json'.format(type, name), 'r') as f:
-            return json.load(f)
+            try:
+                return json.load(f)
+            except:
+                # raise SuitamException('failed to load the json file', error_code='JSON_LOAD_FAILED')
+                return None
     def save(self, type, name, data):
         with open('/root/projects/suitam/data/{}/{}.json'.format(type, name), 'w') as f:
-            json.dump(data, f)
+            try:
+                json.dump(data, f)
+            except:
+                raise SuitamException('failed to dump the json file', error_code='JSON_DUMP_FAILED')
     def exists(self, type, name):
         return os.path.isfile('/root/projects/suitam/data/{}/{}.json'.format(type, name))
 
