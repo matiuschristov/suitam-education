91a9c4a0a69c546cc87de99909ad1af07f7849f7
matiuschristov <info@matiuschristov.com>
2024-08-21 08:36:35 +1000

    docs: add documentation outlining view function purpose

--- pages/views.py
+++ pages/views.py
@@ -9,14 +9,32 @@ from pages.exception import *
 from pages.jsonstore import db
 import json
 import urllib
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone, time
 
+# function home()
+    # Arguments
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/' url
+#
 def home(request):
     return redirect('/calendar/')
 
+# function test_color_scheme()
+    # Arguments
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/test/color-scheme/' url which is for test purposes
+#
 def test_color_scheme(request):
     return render(request, 'color-scheme.html')
 
+# function user_login()
+    # Arguments
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/auth/login/' url which handles all login matters
+#
 def user_login(request):
     if request.method == 'POST':
         login_details = urllib.parse.parse_qs(request.body.decode("utf-8"))
@@ -35,12 +53,26 @@ def user_login(request):
     else:
         return render(request, 'login.html')
 
+# function user_logout()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/auth/logout/' url which clears the authentication cookie
+#
 @require_authentication
 def user_logout(request, user):
     response = redirect('/auth/login/')
     utils.delete_cookie(response, 'adAuthCookie')
     return response
 
+# function app_overview()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/overview/' url which renders an overview page
+#
 @require_authentication
 def app_overview(request, user):
     user['initals'] = "".join(list(map(lambda x: x[0], user.get('name').split(' '))))
@@ -56,6 +88,13 @@ def app_overview(request, user):
         'timetable': overview_timetable_cache
     })
 
+# function app_calendar()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/calendar/' url which renders a 5-day calendar page
+#
 @require_authentication
 def app_calendar(request, user):
     user['initals'] = "".join(list(map(lambda x: x[0], user.get('name').split(' '))))
@@ -65,7 +104,7 @@ def app_calendar(request, user):
         timetable_week = []
         i = 0;
         while len(timetable_week) < 6:
-            timetable_date = datetime.utcnow() + timedelta(days=i,hours=10)
+            timetable_date = datetime.utcnow() + timedelta(days=i)
             i += 1
             if timetable_date.weekday() >= 5:
                 continue;
@@ -106,6 +145,13 @@ def app_calendar(request, user):
         'classes': calendar_classes_cache
     })
 
+# function api_update_class_color()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/class/color/' url which updates the class color in database
+#
 @require_authentication
 def api_update_class_color(request, user):
     user_guid = user.get('guid')
@@ -131,12 +177,26 @@ def api_update_class_color(request, user):
         print('An exception occurred')
     return HttpResponse(status=200)
 
+# function user_profile()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/profile/' url which renders the user profile page
+#
 @require_authentication
 def user_profile(request, user):
     return render(request, 'user.html', {
         'user': user
     })
 
+# function class_resources()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/class/resources/' url which reponds with json class resources
+#
 @require_authentication
 def class_resources(request, user):
     data = intranet.class_resources(request)
@@ -145,19 +205,47 @@ def class_resources(request, user):
         user_classes.append(user_class.get('SubjectDescription'))
     return HttpResponse(json.dumps(data), content_type='text/plain')
 
+# function api_user_information()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/user/information/' url which reponds with json user information
+#
 @require_authentication
 def api_user_information(request, user):
     return HttpResponse(json.dumps(intranet.user_information(request)), content_type='application/json')
 
+# function api_user_photo()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/user/photo/' url which reponds with binary user profile image
+#
 @require_authentication
 def api_user_photo(request, user):
     return HttpResponse(intranet.user_photo(request, user.get('guid')), content_type='image/jpg')
 
+# function api_dashboard_data()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/dashboard/data/' url which reponds with json dashboard data
+#
 @require_authentication
 def api_dashboard_data(request, user):
     data_user_dashboard = intranet.user_dashboard_data(request, guidString=user.get('guid'))
     return HttpResponse(json.dumps(data_user_dashboard), content_type='application/json')
 
+# function api_user_parents()
+    # Arguments
+        # @require_authentication = user must be logged in to access this path
+        # request = the HttpResponse object which is supplied in the view function
+    # Description
+        # the view with corresponds with the '/api/user/parents/' url which reponds with json user parents
+#
 @require_authentication
 def api_user_parents(request, user):
     data_user_dashboard = intranet.user_dashboard_data(request, guidString=user.get('guid'))
