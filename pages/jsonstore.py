49df96d486075fd8514f1e3030baa5e040cc7a6a
matiuschristov <info@matiuschristov.com>
2024-08-21 08:57:32 +1000

    perf: get root path from variable

--- pages/jsonstore.py
+++ pages/jsonstore.py
@@ -2,25 +2,24 @@ import json
 import os
 from pages.exception import *
 
+jsonStorePath = '/root/projects/suitam/data'
+
 class JSONStore():
     def __init__(self):
         pass
     def get(self, type, name):
-        with open('/root/projects/suitam/data/{}/{}.json'.format(type, name), 'r') as f:
+        with open('{}/{}/{}.json'.format(jsonStorePath, type, name), 'r') as f:
             try:
                 return json.load(f)
             except:
-                # raise SuitamException('failed to load the json file', error_code='JSON_LOAD_FAILED')
                 return None
     def save(self, type, name, data):
-        with open('/root/projects/suitam/data/{}/{}.json'.format(type, name), 'w') as f:
+        with open('{}/{}/{}.json'.format(jsonStorePath, type, name), 'w') as f:
             try:
                 json.dump(data, f)
             except:
                 raise SuitamException('failed to dump the json file', error_code='JSON_DUMP_FAILED')
     def exists(self, type, name):
-        return os.path.isfile('/root/projects/suitam/data/{}/{}.json'.format(type, name))
-
-# os.rename('a.txt', 'b.kml')
+        return os.path.isfile('{}/{}/{}.json'.format(jsonStorePath, type, name))
 
 db = JSONStore()
